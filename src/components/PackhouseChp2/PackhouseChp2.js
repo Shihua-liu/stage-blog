801db91c92ff84ee1ad344cf7f570e1428b6141b
shihua liu <70955078+Shihua-liu@users.noreply.github.com>
2023-09-04 11:25:39 +0200

    getting the right data2 rn

--- src/components/PackhouseChp2/PackhouseChp2.js
+++ src/components/PackhouseChp2/PackhouseChp2.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Data2 from "../../Data"
+import Data2 from "../../Data2"
 import { useLocation, useNavigate } from "react-router-dom"
 
 import "./Packhouse.css"
